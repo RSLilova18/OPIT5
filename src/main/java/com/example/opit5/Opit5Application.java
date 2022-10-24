aa6a3ab684457c5b5b0d4c94e048c747f9e31ba3
RSLilova18 <RSLilova18@codingburgas.bg>
2022-10-24 12:03:54 +0300

    Initial commit

---
+++ src/main/java/com/example/opit5/Opit5Application.java
@@ -0,0 +1,13 @@
+package com.example.opit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Opit5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/opit5/Opit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Opit5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
